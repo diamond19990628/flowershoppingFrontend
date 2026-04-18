d7cf5ddd13c08b3a1089e0da90770d39a48d0338
diamond19990628 <yaoyupeng009@gmail.com>
2026-04-19 03:34:53 +0900

    设置了一个登录后就检查session的功能

--- app.js
+++ app.js
@@ -13,4 +13,30 @@ App({
       global: true, // 设置为全局生效,
     });
   },
+  onShow(){
+    wx.request({
+      url:config.BASE_URL+"/login/verify",
+      method:"POST",
+      header: {
+        "Content-Type": "application/json",
+        "token": wx.getStorageSync("token"),
+        "Cookie": "JSESSIONID=" + wx.getStorageSync("JSESSIONID")
+      },
+      success:(res)=>{
+        
+        switch(res.statusCode){
+          case 200:
+            const app = getApp();
+            app.globalData.userInfo = res.data.data;
+            app.globalData.isLogined = true;
+          break;
+          case 401:
+            const app1 = getApp();
+            app1.globalData.userInfo = null;
+            app1.globalData.isLogined = false;
+          break;
+        }
+      }
+    })
+  }
 })
