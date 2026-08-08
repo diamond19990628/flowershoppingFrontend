1eba10465b43309a34858cdcd8e568fa85d8b10b
diamond19990628 <yaoyupeng009@gmail.com>
2026-08-09 01:14:58 +0900

    上传了dev的配置

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
-  BASE_URL: "https://api.tshyshopping.cn",
-  BASE_URL_IMG:"https://img.tshyshopping.cn"
-  // BASE_URL: "http://127.0.0.1:8080"
+  // BASE_URL: "https://api.tshyshopping.cn",
+  // BASE_URL_IMG:"https://img.tshyshopping.cn"
+  BASE_URL: "http://127.0.0.1:8080"
 };
 
 module.exports = config;
